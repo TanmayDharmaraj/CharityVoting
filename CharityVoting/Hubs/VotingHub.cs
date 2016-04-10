84daad66b2710484a069a7614fa609102599ac8b
Tanmay Dharmaraj <get2tanmay@gmail.com>
2016-04-11 01:12:44 +0530

    UI Improvements. Generalized models.

--- CharityVoting/Hubs/VotingHub.cs
+++ CharityVoting/Hubs/VotingHub.cs
@@ -12,27 +12,30 @@ namespace CharityVoting.Hubs
 {
     public class VotingHub : Hub
     {
-        public static int numVotesTanmayDharmaraj = 0;
-        public static int numVotesDonaldTrump = 0;
+        public static int numVotesVote1 = 0;
+        public static int numVotesVote2 = 0;
+        public static int numVotesVote3 = 0;
 
         public override Task OnConnected()
         {
-            Clients.All.upVote(new Vote() { TanmayDharmaraj = numVotesTanmayDharmaraj, DonaldTrump = numVotesDonaldTrump });
+            Clients.All.upVote(new Vote() { Vote1 = numVotesVote1, Vote2 = numVotesVote2, Vote3 = numVotesVote3 });
             return base.OnConnected();
         }
         public void UpVote(string myvote)
         {
-            
-            if(myvote == "TanmayDharmaraj")
+            switch (myvote)
             {
-                numVotesTanmayDharmaraj++;
+                case "Vote1":
+                    numVotesVote1++;
+                    break;
+                case "Vote2":
+                    numVotesVote2++;
+                    break;
+                case "Vote3":
+                    numVotesVote3++;
+                    break;
             }
-            else if(myvote == "DonaldTrump")
-            {
-                numVotesDonaldTrump++;
-            }
-            
-            Clients.All.upVote(new Vote() { TanmayDharmaraj = numVotesTanmayDharmaraj, DonaldTrump = numVotesDonaldTrump });
+            Clients.All.upVote(new Vote() { Vote1 = numVotesVote1, Vote2 = numVotesVote2, Vote3 = numVotesVote3 });
         }
     }
 }
--- CharityVoting/Models/Vote.cs
+++ CharityVoting/Models/Vote.cs
@@ -8,10 +8,13 @@ namespace CharityVoting.Models
 {
     public class Vote
     {
-        [JsonProperty("TanmayDharmaraj")]
-        public int TanmayDharmaraj { get; set; }
+        [JsonProperty("Vote1")]
+        public int Vote1 { get; set; }
 
-        [JsonProperty("DonaldTrump")]
-        public int DonaldTrump { get; set; }
+        [JsonProperty("Vote2")]
+        public int Vote2{ get; set; }
+
+        [JsonProperty("Vote3")]
+        public int Vote3 { get; set; }
     }
 }
